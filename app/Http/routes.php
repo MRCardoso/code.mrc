8fc062a918d651bd41282625d838edb44ddd89c6
MRCardoso <cardozinhos.mrc@gmail.com>
2015-07-28 23:33:04 -0300

    corrections

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,16 @@
 |
 */
 
+// ocorre um erro quando clono o repositorio, quando rodo o o composer update gera esse erro:
+
+    // Script php artisan clear-compiled handling the pre-update-cmd event returned with an error
+
+// mas se eu rodo o 'composer update --no-scripts' e depois o composer update funciona
+// porem nao roda o trecho scripts do composer.json onde cria o arquivo .env e gera a key do artisan
+// desse modo tenho que gerar os comando na mao
+// copiar o .env.example e rodar um php artisan key:generate
+// sabe o motivo do erro?
+
 Route::get('/', function () {
     return view('welcome');
 });
